5c2bcb2256edde97f031b09cf66c9045a688b8fe
Vegard Solheim Theriault <vegather@icloud.com>
2016-05-26 21:02:03 +0200

    Initial Commit

---
+++ Bezier Curve/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bezier Curve
+//
+//  Created by Vegard Solheim Theriault on 26/05/16.
+//  Copyright © 2016 Vegard Solheim Theriault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
